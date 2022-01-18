0bce1cf1e7c04001bfd680b574574dadf13902cd
Felipe Jansen <felipe.jansen@outlook.com>
2022-01-17 21:42:53 -0300

    first commit

---
+++ src/test/java/com/example/jenkinsjavaparameterized/JenkinsJavaParameterizedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsjavaparameterized;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsJavaParameterizedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
