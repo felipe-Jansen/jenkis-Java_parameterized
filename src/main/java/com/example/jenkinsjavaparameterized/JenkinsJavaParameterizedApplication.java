7941d3abef517bad7ea5ba271378dd58461a88ae
Felipe Jansen <felipe.jansen@outlook.com>
2022-01-17 22:21:36 -0300

    JenkinsFile add

--- src/main/java/com/example/jenkinsjavaparameterized/JenkinsJavaParameterizedApplication.java
+++ src/main/java/com/example/jenkinsjavaparameterized/JenkinsJavaParameterizedApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsJavaParameterizedApplication {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome, " + args[0]);
     }
 
 }
